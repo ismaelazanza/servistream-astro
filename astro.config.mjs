fa1263f38062fa7beba92f9e35996a6713dc42a1
ismaelazanza <ismaelazanza@gmail.com>
2025-10-01 20:45:47 -0500

    9.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://servistream.net',
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      filter: (page) => !page.includes('/admin') && !page.includes('/private')
+    })
+  ]
 });
